d7c90709ccf7c59b19ac207abf92acdb5595cd36
renosyah <syahputrareno975@gmail.com>
2022-12-10 15:10:31 +0700

    handle error on request scoreboard

--- asset/scoreboard/scoreboard.gd
+++ asset/scoreboard/scoreboard.gd
@@ -3,6 +3,12 @@ extends Node
 onready var _http_request = $HTTPRequest
 onready var holder = $VBoxContainer/ScrollContainer/VBoxContainer
 
+onready var scroll_container = $VBoxContainer/ScrollContainer
+onready var loading_text = $VBoxContainer/loading_text
+onready var error_text = $VBoxContainer/error_text
+onready var empty_text = $VBoxContainer/empty_text
+
+
 var offset :int = 0
 var limit :int = 10
 
@@ -11,6 +17,11 @@ func _ready():
 	get_scoreboard(offset, limit)
 	
 func get_scoreboard(offset, limit :int):
+	scroll_container.visible = false
+	loading_text.visible = true
+	error_text.visible = false
+	empty_text.visible = false
+	
 	var url = Global.base_url + "/api/score/list.php"
 	var query = JSON.print({
 		"search_by": "game_id",
@@ -23,7 +34,15 @@ func get_scoreboard(offset, limit :int):
 	var headers = ["Content-Type: application/json"]
 	_http_request.request(url, headers, false, HTTPClient.METHOD_POST, query)
 	
-func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+func _on_HTTPRequest_request_completed(result:int, response_code:int, headers:PoolStringArray, body:PoolByteArray):
+	scroll_container.visible = true
+	loading_text.visible = false
+	
+	if result != HTTPRequest.RESULT_SUCCESS:
+		scroll_container.visible = false
+		error_text.visible = true
+		return
+		
 	var json :JSONParseResult = JSON.parse(body.get_string_from_utf8())
 	if not json.result is Dictionary:
 		return
@@ -31,7 +50,13 @@ func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	for i in holder.get_children():
 		holder.remove_child(i)
 		i.queue_free()
+
 	
+	if json.result["data"].empty():
+		scroll_container.visible = false
+		empty_text.visible = true
+		return
+		
 	var pos :int = 1
 	for i in json.result["data"]:
 		var item = preload("res://asset/scoreboard/item/item.tscn").instance()
